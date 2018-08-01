186d2312d073c47697f2a237af5f1365dfda473b
BlaiseAK <blaise.a.kress@gmail.com>
2018-08-01 10:32:28 -0400

    added the navbar.js and header.js files to the app.js and linked them, css will be done in the js files (note to self to remove all .css files

---
+++ clickygame/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => (
+    <header>
+        <h1>
+            A Game of Clicks!
+        </h1>
+        <h4>Select a different houses</h4>
+    </header>
+)
+
+export default Header
---
+++ clickygame/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+
+const Navbar = () => (
+    <div>
+        <nav>
+            <ul className="unstyled">
+                <li><strong>A Game of Clicks</strong></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </div>
+)
+
+export default Navbar
