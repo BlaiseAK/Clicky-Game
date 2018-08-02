bf4f07ca10130ed17c7962631eae15a1c644aa3e
BlaiseAK <blaise.a.kress@gmail.com>
2018-08-01 22:06:16 -0400

    got things styled in the navbar but need to work on the height and fixed positioning

--- clickygame/src/components/Header.js
+++ clickygame/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+
+
+
+
 const Header = () => (
     <header>
         <h1>
             A Game of Clicks!
         </h1>
-        <h4>Select a different houses</h4>
+        <h4>Select a different house</h4>
     </header>
 )
 
